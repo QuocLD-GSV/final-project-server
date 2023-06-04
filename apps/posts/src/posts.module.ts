2e52ddff4056e5f58ddb5538a600547c7ff4d4b4
quocld <quocle2208@gmail.com>
2023-06-04 18:21:18 +0700

    feat: database config

--- apps/posts/src/posts.module.ts
+++ apps/posts/src/posts.module.ts
@@ -1,9 +1,22 @@
+import { DatabaseModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+      envFilePath: './apps/orders/.env',
+    }),
+    DatabaseModule,
+  ],
   controllers: [PostsController],
   providers: [PostsService],
 })
