f62f86e59b9640364fd2834612583cd7d68e2a8b
quocld <quocle2208@gmail.com>
2023-06-22 04:06:12 +0700

    feat: comment on post

---
+++ apps/posts/src/comments/comments.service.ts
@@ -0,0 +1,46 @@
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { Types } from 'mongoose';
+import { HTTPExceptions } from '../decorators/try-catch';
+import { LikesRepository } from '../repository/likes.repository';
+import { PostsRepository } from '../repository/posts.repository';
+import { CreateCommentToPostDto } from './dto/comment-post.dto';
+import { CommentErrors } from './errors/comments.errors';
+import { CommentsRepository } from './repositorys/comments.repository';
+
+@Injectable()
+export class CommentsService {
+  constructor(
+    private commentsRepository: CommentsRepository,
+    private likesRepository: LikesRepository,
+    private postRepository: PostsRepository,
+  ) {}
+
+  @HTTPExceptions(
+    CommentErrors.INTERNAL_SERVER_ERROR,
+    HttpStatus.INTERNAL_SERVER_ERROR,
+  )
+  async createCommentToPost(
+    data: CreateCommentToPostDto,
+    user_id: Types.ObjectId,
+  ) {
+    const comment = await this.commentsRepository.create({
+      ...data,
+      author_id: user_id,
+    });
+
+    await this.postRepository.findOneAndUpdate(
+      { _id: comment.postId },
+      {
+        $addToSet: {
+          comments: comment._id,
+        },
+      },
+    );
+
+    const returnComment = await this.commentsRepository.returnComment(
+      comment._id,
+    );
+
+    return returnComment;
+  }
+}
