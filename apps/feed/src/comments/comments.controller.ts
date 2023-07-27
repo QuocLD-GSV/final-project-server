78d7a4e244751d8a33a4d29cf36f7c7fdf4f8e53
quocld <quocle2208@gmail.com>
2023-07-27 15:21:30 +0700

    ✨ feat(feed service): all post comments
    get all post comment information

--- apps/feed/src/comments/comments.controller.ts
+++ apps/feed/src/comments/comments.controller.ts
@@ -7,11 +7,19 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import { Payload } from '@nestjs/microservices';
+import { Types } from 'mongoose';
 import { CommentsService } from './comments.service';
-import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
+import { GetAllPostCommentsDto } from './dto/get-all-post-comments';
 
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
+
+  @Get('all-post-comments')
+  async getAllPostComments(@Payload() data: GetAllPostCommentsDto) {
+    return this.commentsService.getAllPostComments(
+      new Types.ObjectId(data.postId),
+    );
+  }
 }
--- apps/feed/src/comments/comments.module.ts
+++ apps/feed/src/comments/comments.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
+import { DatabaseModule } from '@app/common';
+import { CommentsRepository } from '../repository/comments.repository';
 
 @Module({
   controllers: [CommentsController],
-  providers: [CommentsService]
+  providers: [CommentsService, CommentsRepository],
 })
 export class CommentsModule {}
