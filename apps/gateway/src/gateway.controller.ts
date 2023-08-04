c340ce72a5772c50371325b787894ac85592ab2a
quocld <quocle2208@gmail.com>
2023-08-04 17:32:55 +0700

    🎉 init(api gateway): init gateway module
    initial api gateway module and rabbitmq resgister

--- apps/gateway/src/gateway.controller.ts
+++ apps/gateway/src/gateway.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class GatewayController {
-  constructor(s) {}
+  constructor() {}
 
   @Get()
   getHello(): string {
---
+++ libs/common/src/constants/services.constants.ts
@@ -0,0 +1,5 @@
+export const service = {
+  POSTS_SERVICE: 'POSTS',
+  AUTH_SERVICE: 'AUTH',
+  FEED_SERVICE: 'FEED',
+};
