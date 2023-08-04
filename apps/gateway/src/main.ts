46ef67fe1426b8f11fc8be94c954954995f3f5ac
quocld <quocle2208@gmail.com>
2023-08-04 16:59:06 +0700

    ✨ feat(api gateway): add swagger
    add swagger to document api and add configService

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -1,8 +1,23 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { GatewayModule } from './gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
-  await app.listen(5000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Post API')
+    .setDescription('The Posts API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/', app, document);
+
+  const configService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
