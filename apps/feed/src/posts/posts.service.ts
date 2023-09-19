6a948f89323619ee511a9575f7ad038c93bc15b5
quocld <quocld@glory-software.vn>
2023-09-19 18:30:20 +0700

    🐞 fix: return more user infomations

--- apps/feed/src/posts/posts.service.ts
+++ apps/feed/src/posts/posts.service.ts
@@ -39,6 +39,6 @@ export class PostsService {
   }
 
   async getProfileUserPost(userId: Types.ObjectId){
-    return await this.postsRepository.find({user_id: userId});
+    return await this.postsRepository.findAllProfilePost({user_id: userId});
   }
 }
